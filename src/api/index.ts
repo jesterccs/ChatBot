f1e1a782e5383e64a47668b5ca416500bdeab3dc
chamika.c <chamikachathushka8@gmail.com>
2023-06-06 15:34:16 +0530

    Added API class

---
+++ src/api/index.ts
@@ -0,0 +1,18 @@
+
+    export const Test = async (obj: any)=> {
+        const url = 'http://localhost:5000/chat'
+        const postData = {
+            sender: obj.sender,
+            message: obj.message,
+            language: obj.language,
+            button: obj.button
+        }
+
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(postData)
+        })
+        console.log(response)
+        return response.json()
+    }
+
--- src/pages/Chatbot.tsx
+++ src/pages/Chatbot.tsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect, useRef} from "react";
 import "../styles/ChatBot.css";
+import {Test} from "../api";
+
 
 interface Message {
     id: number;
@@ -7,19 +9,30 @@ interface Message {
     sender: "user" | "chatbot";
 }
 
+interface Option {
+    label: string;
+    value: string;
+}
+
 const Chatbot: React.FC = () => {
     const [messages, setMessages] = useState<Message[]>([]);
+    const [newMessage, setNewMessage] = useState<Message>()
     const [inputValue, setInputValue] = useState("");
     const messagesEndRef = useRef<HTMLDivElement>(null);
     const inputRef = useRef<HTMLTextAreaElement>(null);
 
     const [useCaseIsOpen, setUseCaseIsOpen] = useState(false);
-    const [useCaseSelectedOption, setUseCaseSelectedOption] = useState('');
+    const [useCaseSelectedOption, setUseCaseSelectedOption] = useState<Option | null>(null);
     const [languageIsOpen, setLanguageIsOpen] = useState(false)
-    const [languageSelectedOption, setLanguageSelectedOption] = useState('')
+    const [languageSelectedOption, setLanguageSelectedOption] = useState('English')
 
 
-    const useCaseOptions = ['Option 1', 'Option 2', 'Option 3'];
+    // const useCaseOptions = ['Option 1', 'Option 2', 'Email Drafting'];
+    const useCaseOptions: Option[] = [
+        {label: 'option1', value: 'Option 1'},
+        {label: 'option1', value: 'Option 2'},
+        {label: 'emailDrafting', value: 'Email Drafting'},
+    ];
     const languageOptions = ['English', 'Sinhala', 'Tamil']
 
 
@@ -52,11 +65,8 @@ const Chatbot: React.FC = () => {
                 content: inputValue,
                 sender: "user",
             };
-
-            setMessages([...messages, newMessage]);
+            setMessages((prevMessages) => [...prevMessages, newMessage]);
             setInputValue("");
-            console.log(newMessage)
-            console.log(messages)
             if (inputRef.current) {
                 inputRef.current.style.height = "auto";
             }
@@ -75,7 +85,7 @@ const Chatbot: React.FC = () => {
 
 
     const useCaseToggleMenu = () => {
-        setUseCaseIsOpen(!useCaseIsOpen);
+        setUseCaseIsOpen((prevState) => !prevState);
     };
 
     const handleOptionClickUseCase = (option: any) => {
@@ -84,13 +94,27 @@ const Chatbot: React.FC = () => {
     };
 
     const languageToggleMenu = () => {
-        setLanguageIsOpen(!languageIsOpen)
-    }
+        setLanguageIsOpen((prevState) => !prevState);
+    };
 
     const handleOptionsClickLanguage = (language: any) => {
         setLanguageSelectedOption(language)
         setLanguageIsOpen(false)
     }
+
+    // const handleOnClick = async () => {
+    //     console.log(languageSelectedOption)
+    //     console.log(">>>>>>>>>>>>>>>>>> : " + useCaseSelectedOption?.value)
+    //     const obj = {
+    //         sender: newMessage.sender,
+    //         message: newMessage.content,
+    //         language: languageSelectedOption,
+    //         button: useCaseSelectedOption?.value
+    //     }
+    //     await Test(obj)
+    //
+    // }
+
 
     return (
         <div className="container">
@@ -123,13 +147,13 @@ const Chatbot: React.FC = () => {
                         <label className="useCase-dropdown-label">Select a Use Case</label>
                         <div className="useCase-dropdown">
                             <button className="useCase-dropdown-toggle" onClick={useCaseToggleMenu}>
-                                {useCaseSelectedOption ? useCaseSelectedOption : useCaseOptions[0]}
+                                {useCaseSelectedOption ? useCaseSelectedOption.value : useCaseOptions[0].value}
                             </button>
                             {useCaseIsOpen && (
                                 <ul className="useCase-dropdown-menu">
                                     {useCaseOptions.map((option, index) => (
                                         <li key={index} onClick={() => handleOptionClickUseCase(option)}>
-                                            {option}
+                                            {option.value}
                                         </li>
                                     ))}
                                 </ul>
@@ -143,17 +167,17 @@ const Chatbot: React.FC = () => {
                 {/*Message container*/}
                 <div className="message-container">
                     <div className="chatbot-messages">
-                        {messages.map((message) => (
-                            <React.Fragment key={message.id}>
-                                <div
-                                    className={`message ${message.sender === "user" ? "user" : "chatbot"}`}
-                                    style={{whiteSpace: "pre-wrap"}}
-                                >
-                                    {message.content}
-                                </div>
-                                <div>response</div>
-                            </React.Fragment>
-                        ))}
+                        {messages.map((message) => {
+                            console.log(`Rendering message with id: ${message.id} ... ${message.content}`);
+                            return (
+                                <>
+                                    <div key={message.id} className={`message ${message.sender}`}>
+                                        {message.content}
+                                    </div>
+                                    <div>response</div>
+                                </>
+                            )
+                        })}
                         <div ref={messagesEndRef}/>
                     </div>
 
